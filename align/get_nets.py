ffbe136eebb6f4bb5eba47f4802a3c6cf0fd9b86
z14git <lzl1992@gmail.com>
2019-12-28 11:54:55 +0800

    获取当前脚本所在目录，方便在notebook中调用

--- align/get_nets.py
+++ align/get_nets.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from collections import OrderedDict
 import numpy as np
+import os
 
 
 class Flatten(nn.Module):
@@ -51,8 +52,8 @@ class PNet(nn.Module):
 
         self.conv4_1 = nn.Conv2d(32, 2, 1, 1)
         self.conv4_2 = nn.Conv2d(32, 4, 1, 1)
-
-        weights = np.load("./pnet.npy", allow_pickle=True)[()]
+        cur_dir = os.path.split(os.path.realpath(__file__))[0]
+        weights = np.load(os.path.join(cur_dir, "pnet.npy"), allow_pickle=True)[()]
         for n, p in self.named_parameters():
             p.data = torch.FloatTensor(weights[n])
 
@@ -96,8 +97,8 @@ class RNet(nn.Module):
 
         self.conv5_1 = nn.Linear(128, 2)
         self.conv5_2 = nn.Linear(128, 4)
-
-        weights = np.load("./rnet.npy", allow_pickle=True)[()]
+        cur_dir = os.path.split(os.path.realpath(__file__))[0]
+        weights = np.load(os.path.join(cur_dir, "rnet.npy"), allow_pickle=True)[()]
         for n, p in self.named_parameters():
             p.data = torch.FloatTensor(weights[n])
 
@@ -147,8 +148,8 @@ class ONet(nn.Module):
         self.conv6_1 = nn.Linear(256, 2)
         self.conv6_2 = nn.Linear(256, 4)
         self.conv6_3 = nn.Linear(256, 10)
-
-        weights = np.load("./onet.npy", allow_pickle=True)[()]
+        cur_dir = os.path.split(os.path.realpath(__file__))[0]
+        weights = np.load(os.path.join(cur_dir, "onet.npy"), allow_pickle=True)[()]
         for n, p in self.named_parameters():
             p.data = torch.FloatTensor(weights[n])
 
